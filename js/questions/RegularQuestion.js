68a0314fd4eedb0ffafb08dc4f62919c78a8d95c
vaidhy31 <vaidhy.naganathan@gmail.com>
2026-08-18 22:58:47 +0530

    Ver 9.2

--- js/questions/RegularQuestion.js
+++ js/questions/RegularQuestion.js
@@ -1,11 +1,1 @@
-
-import { esc } from "../utils/helpers.js";
-
-export function renderRegularQuestion(question, game) {
-  const answer = question.answer;
-  return `
-    <div class="regular-answer-area">
-      <div class="muted">ANSWER</div>
-      <div class="regular-answer">${game.revealed ? esc(answer) : "&nbsp;"}</div>
-    </div>`;
-}
+Unsupported Media Type
